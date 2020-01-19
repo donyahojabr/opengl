b6bf56211fb72c5074e1a46de87796d246b8926c
donyahojabr <donya.hojabr@mail.mcgill.ca>
2020-01-19 18:40:31 -0500

    Initial Commit

---
+++ OpenGLCourseApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLCourseApp
+//
+//  Created by Donya Hojabr on 2020-01-19.
+//  Copyright © 2020 Donya Hojabr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
