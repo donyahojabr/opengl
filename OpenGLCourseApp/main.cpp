d256279912f1d160d000430469f303da1b9d9532
donyahojabr <donya.hojabr@mail.mcgill.ca>
2020-01-19 22:16:01 -0500

    inital commit

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -1,15 +1,50 @@
-//
-//  main.cpp
-//  OpenGLCourseApp
-//
-//  Created by Donya Hojabr on 2020-01-19.
-//  Copyright © 2020 Donya Hojabr. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+//GLFW
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main(){
+    glfwInit( );
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //need this for mac
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL",  nullptr, nullptr);
+    
+    int screenWidth, screenHeight;
+    
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if(nullptr == window){
+        std::cout <<"Failed to create window"<<std:: endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    glewInit();
+    glViewport(0, 0, screenWidth, screenHeight);
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        
+    }
+    glfwTerminate();
     return 0;
+    
 }
+
+
+
+
+
